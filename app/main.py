fa786a372e4359f818c2d9c16bbac59690559c02
AbdelrahmanHaloda <AbdelrahmanHaloda@gmail.com>
2026-07-13 16:54:59 +0300

    Add endpoints reuquest - create temp dictionary for database handeling - add post request for posts - add get request for posts - add responsive models for both

--- app/main.py
+++ app/main.py
@@ -3,7 +3,24 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item(BaseModel):
+class UserPostIn(BaseModel):
     Body: str
 
+class UserPost(UserPostIn):
+    id: int
 
+# database
+post_table = {}
+
+@app.post("/post", response_model = UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    lastPostId = len(post_table)
+    new_post = {**data, "id": lastPostId + 1}
+    post_table[lastPostId] = new_post
+    return new_post
+
+
+@app.get("/posts", response_model = list[UserPost])
+async def get_posts():
+    return list(post_table.values())
